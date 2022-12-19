1bbddbf686ea6b46c022d85ce0045d9aab3fb501
CptPerebey <116084446+CptPerebey@users.noreply.github.com>
2022-12-19 16:11:02 +0300

    ДЗ Хироку

--- src/test/java/DownLoad.java
+++ src/test/java/DownLoad.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -10,6 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.List;
 
 public class DownLoad {
@@ -26,15 +28,19 @@ public class DownLoad {
     @Test
     public void testDownLoad() throws InterruptedException {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", System.getProperty("user.dir"));
+        ChromeOptions options = new ChromeOptions();
+        options.setExperimentalOption("prefs", chromePrefs);
+        driver = new ChromeDriver(options);
         WebDriverWait wait = new WebDriverWait(driver, 20);
         driver.get("http://the-internet.herokuapp.com/download");
         List<WebElement> list = driver.findElements(By.xpath("//div[@class=\"example\"]//a"));
         WebElement elem = list.get(list.size() - 3);
         elem.click();
         Thread.sleep(500);
-        File folder = new File(System.getProperty("user.dir")+ "/sample.png");
+        File folder = new File(System.getProperty("user.dir"));
         File[] listOfFiles = folder.listFiles();
         boolean found = false;
         File f = null;
@@ -42,7 +48,7 @@ public class DownLoad {
             if (listOfFile.isFile()) {
                 String fileName = listOfFile.getName();
                 System.out.println("File " + listOfFile.getName());
-                if (fileName.matches("sample.png")) {
+                if (fileName.matches("ANFR-spectre-frequences-juin-2020.pdf")) {
                     f = new File(fileName);
                     found = true;
                 }
